54706ce577ba3e128360e0f5321e78898ec03021
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-04-09 13:06:01 +0400

    [Feature] Featured Jobs (#8)

--- src/components/organisms/Jobs/Jobs.jsx
+++ src/components/organisms/Jobs/Jobs.jsx
@@ -21,10 +21,24 @@ export default function Jobs() {
         </div>
         <div className={styles.jobsContainerRightSide}>
           <button className={styles.embla__next} onClick={scrollPrev}>
-            <img src="/images/svg/Arrow.svg" alt="Arrow" />
+            <svg className={styles.leftButton}>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5.24264 11.8033L0.46967 7.03031C0.176777 6.73741 0.176777 6.26254 0.46967 5.96965L5.24264 1.19668C5.53553 0.903784 6.01041 0.903784 6.3033 1.19668C6.59619 1.48957 6.59619 1.96444 6.3033 2.25734L2.81066 5.74998H14C14.4142 5.74998 14.75 6.08576 14.75 6.49998C14.75 6.91419 14.4142 7.24998 14 7.24998H2.81066L6.3033 10.7426C6.59619 11.0355 6.59619 11.5104 6.3033 11.8033C6.01041 12.0962 5.53553 12.0962 5.24264 11.8033Z"
+                fill="#152137"
+              />
+            </svg>
           </button>
           <button className={styles.embla__prev} onClick={scrollNext}>
-            <img src="/images/svg/Arrow.svg" alt="Arrow" />
+            <svg className={styles.rightButton}>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5.24264 11.8033L0.46967 7.03031C0.176777 6.73741 0.176777 6.26254 0.46967 5.96965L5.24264 1.19668C5.53553 0.903784 6.01041 0.903784 6.3033 1.19668C6.59619 1.48957 6.59619 1.96444 6.3033 2.25734L2.81066 5.74998H14C14.4142 5.74998 14.75 6.08576 14.75 6.49998C14.75 6.91419 14.4142 7.24998 14 7.24998H2.81066L6.3033 10.7426C6.59619 11.0355 6.59619 11.5104 6.3033 11.8033C6.01041 12.0962 5.53553 12.0962 5.24264 11.8033Z"
+                fill="#152137"
+              />
+            </svg>
           </button>
         </div>
       </div>

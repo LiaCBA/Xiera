188604fe9a10c73dca37402ec34cb0377893e180
LiaCBA <moonlightt1083@gmail.com>
2025-04-13 20:02:39 +0400

    feature/newsLetter-11

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/organisms/Footer/Footer";
 import Header from "../../components/organisms/Header/Header";
+import NewsLetter from "../../components/organisms/NewsLetter/NewsLetter";
 
 const Home = () => {
   return (
@@ -23,7 +24,7 @@ const Home = () => {
       {/*Testimonials near me component import here*/}
 
       {/*Subscribe newsletter*/}
-      {/*Subscribe newsletter component import here*/}
+      <NewsLetter />
 
       {/*Footer*/}
       <Footer />
